e0bf3907026a9905fba80a76d22a92bda824273f
AiswaryalakshmiG <abbas.m@fingress.com>
2025-04-10 18:35:48 +0530

    updated new  files

--- login.js
+++ login.js
@@ -2,8 +2,6 @@ function validatelogin (){
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     if(username ===" "|| password ===" "){
-        // console.log("Username: " + username.value);
-        // console.log("password: " + password.value);
         alert("Enter username and passord");
         return false;
     }
