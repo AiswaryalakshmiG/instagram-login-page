451eb01555a0edd1325a260bc1124165c5525130
AishuTechie <aiswaryalakshmi1502@gmail.com>
2025-04-14 20:43:40 +0530

    Updated files

--- login.js
+++ login.js
@@ -4,10 +4,17 @@ document.getElementById("loginForm").addEventListener("submit", function (event)
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
 
-    const savedUsername = localStorage.getItem("signupUsername");
-    const savedPassword = localStorage.getItem("signupPassword");
 
-    if (username === savedUsername && password === savedPassword) {
+    // Get all registered users from localStorage
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    // Check if user exists in array
+    const matchedUser = users.find(user => user.username === username && user.password === password);
+
+    // const savedUsername = localStorage.getItem("signupUsername");
+    // const savedPassword = localStorage.getItem("signupPassword");
+
+    if (matchedUser) {
         alert("Login successful!");
         window.location.href = "homepage.html";
     } else {
